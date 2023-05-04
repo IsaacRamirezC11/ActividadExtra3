41a8a4925dba05d899ec952814d406e84ebf3f61
IsaacRamirezC11 <isaaccortes08@aragon.unam.mx>
2023-05-04 17:21:57 -0600

    Se agrega el proyecto

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Ventana ventana = new Ventana("Layouts");
+        VentanaGrind ventanaGrind = new VentanaGrind("Ventana Layout cuadriculada");
+
+
+    }
+}
